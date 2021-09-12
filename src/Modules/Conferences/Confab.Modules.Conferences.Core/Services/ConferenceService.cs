833235c84d10220b965e78a7ed80b3367245b544
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-09-12 06:07:42 +0200

    Conference repository

--- src/Modules/Conferences/Confab.Modules.Conferences.Core/Services/ConferenceService.cs
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Services/ConferenceService.cs
@@ -2,14 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Confab.Modules.Conferences.Core.DTO;
+using Confab.Modules.Conferences.Core.Entities;
+using Confab.Modules.Conferences.Core.Exceptions;
+using Confab.Modules.Conferences.Core.Repositories;
 
 namespace Confab.Modules.Conferences.Core.Services
 {
     internal class ConferenceService : IConferenceService
     {
-        public Task AddAsync(ConferenceDto dto)
+        private readonly IConferenceRepository _conferenceRepository;
+        private readonly IHostRepository _hostRepository;
+
+        public ConferenceService(IConferenceRepository conferenceRepository, IHostRepository hostRepository)
         {
-            throw new NotImplementedException();
+            _conferenceRepository = conferenceRepository;
+            _hostRepository = hostRepository;
+        }
+        
+        public async Task AddAsync(ConferenceDto dto)
+        {
+            if (await _hostRepository.GetAsync(dto.HostId) is null)
+            {
+                throw new HostNotFoundException(dto.Id);
+            }
+
+            dto.Id = Guid.NewGuid();
+            await _conferenceRepository.AddAsync(new Conference()
+            {
+                Id = dto.Id,
+                HostId = dto.HostId,
+                Description = dto.Desc
+            });
         }
 
         public Task<ConferenceDetailsDto> GetAsync(Guid id)
