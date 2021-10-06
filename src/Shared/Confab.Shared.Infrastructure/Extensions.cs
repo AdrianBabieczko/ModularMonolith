cdf0e8dba90101306a6c9a80d00cedaff3557533
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-10-06 17:51:21 +0200

    Prepare for postgres

--- src/Shared/Confab.Shared.Infrastructure/Extensions.cs
+++ src/Shared/Confab.Shared.Infrastructure/Extensions.cs
@@ -1,10 +1,12 @@
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 using Confab.Shared.Abstractions;
 using Confab.Shared.Infrastructure.Api;
 using Confab.Shared.Infrastructure.Exceptions;
 using Confab.Shared.Infrastructure.Time;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly:InternalsVisibleTo("Confab.Bootstrapper")]
@@ -36,6 +38,20 @@ namespace Confab.Shared.Infrastructure
             });
             
             return app;
+        }
+
+        public static T GetOptions<T>(this IServiceCollection services, string sectionName) where T: new()
+        {
+            using var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return configuration.GetOptions<T>(sectionName);
+        }
+
+        private static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : new()
+        {
+            var options = new T();
+            configuration.GetSection(sectionName).Bind(options);
+            return options;
         }
     }
 }
---
+++ src/Shared/Confab.Shared.Infrastructure/Postgres/PostgresOptions.cs
@@ -0,0 +1,7 @@
+namespace Confab.Shared.Infrastructure.Postgres
+{
+    internal class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
