d229d051e8df56c9ef42f3a8ae1fb7e52a1d60ce
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-11-12 15:18:19 +0100

    Module loader

--- src/Bootstrapper/Confab.Bootstrapper/Startup.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Startup.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using Confab.Modules.Conferences.Api;
+using Confab.Shared.Abstractions.Modules;
 using Confab.Shared.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,10 +12,18 @@ namespace Confab.Bootstrapper
 {
     public class Startup
     {
+        private readonly IList<Assembly> _assemblies;
+        private readonly IList<IModule> _modules;
+
+        public Startup()
+        {
+            _assemblies = ModuleLoader.LoadAssemblies();
+            _modules = ModuleLoader.LoadModules(_assemblies);
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructure();
-            services.AddConferences();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
