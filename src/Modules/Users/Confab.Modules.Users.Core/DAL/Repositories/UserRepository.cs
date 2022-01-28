ba3e838a9ff79776d2601f3682ad6cebc41e618f
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-01-28 22:38:10 +0100

    User module dtos

--- src/Modules/Users/Confab.Modules.Users.Core/DAL/Repositories/UserRepository.cs
+++ src/Modules/Users/Confab.Modules.Users.Core/DAL/Repositories/UserRepository.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Confab.Modules.Users.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +15,22 @@ namespace Confab.Modules.Users.Core.DAL.Repositories
             _context = context;
             _users = _context.Users;
         }
+
+        public Task<User> GetAsync(Guid id) => _users.SingleOrDefaultAsync(x => x.Id == id);
+
+        public Task<User> GetAsync(string email) => _users.SingleOrDefaultAsync(x => x.Email == email);
+
+        public async Task AddAsync(User user)
+        {
+            await _users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        
     }
 }
--- src/Modules/Users/Confab.Modules.Users.Core/DTO/SignInDto.cs
+++ src/Modules/Users/Confab.Modules.Users.Core/DTO/SignInDto.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Confab.Modules.Users.Core.DTO
 {
     public class SignInDto
     {
-        //TODO: implement!
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        
+        [Required] 
+        public string Password { get; set; }
     }
 }
