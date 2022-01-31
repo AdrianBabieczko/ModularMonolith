a16dbb8d7d7b31a9da0f01774e577f6fd2ea3b56
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-01-31 22:04:12 +0100

    Identity service

--- src/Modules/Users/Confab.Modules.Users.Core/Services/IdentityService.cs
+++ src/Modules/Users/Confab.Modules.Users.Core/Services/IdentityService.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Confab.Modules.Users.Core.DTO;
 using Confab.Modules.Users.Core.Entities;
+using Confab.Modules.Users.Core.Exceptions;
 using Confab.Modules.Users.Core.Repositories;
 using Confab.Shared.Abstractions.Auth;
 using Confab.Shared.Abstractions.Messaging;
@@ -16,15 +18,56 @@ namespace Confab.Modules.Users.Core.Services
         private readonly IPasswordHasher<User> _passwordHasher;
         private readonly IAuthManager _authManager;
         private readonly IClock _clock;
-        private readonly IMessageBroker _messageBroker;
-        public Task<AccountDto> GetAsync(Guid id)
+        //private readonly IMessageBroker _messageBroker;
+
+        public IdentityService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, IAuthManager authManager, IClock clock)
         {
-            throw new NotImplementedException();
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _authManager = authManager;
+            _clock = clock;
+        }
+        public async Task<AccountDto> GetAsync(Guid id)
+        {
+            var user = await _userRepository.GetAsync(id);
+
+            return user is null
+                ? null
+                : new AccountDto
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = user.Claims,
+                    Role = user.Role,
+                    CreatedAt = user.CreatedAt
+                };
         }
 
-        public Task SignUpAsync(SingUpDto dto)
+        public async Task SignUpAsync(SingUpDto dto)
         {
-            throw new NotImplementedException();
+            dto.Id = Guid.NewGuid();
+            var email = dto.Email.ToLowerInvariant();
+            var user = await _userRepository.GetAsync(email);
+            
+            if (user is not null)
+            {
+                throw new EmailInUseException();
+            }
+
+            var password = _passwordHasher.HashPassword(default, dto.Password);
+
+            user = new User
+            {
+                Id = dto.Id,
+                Email = email,
+                Claims = dto.Claims ?? new Dictionary<string, IEnumerable<string>>(),
+                Password = password,
+                Role = dto.Role?.ToLowerInvariant() ?? "user",
+                CreatedAt = _clock.CurrentDate(),
+                IsActive = true
+            };
+
+            await _userRepository.AddAsync(user);
         }
 
         public Task<JsonWebToken> SignInAsync(SignInDto dto)
