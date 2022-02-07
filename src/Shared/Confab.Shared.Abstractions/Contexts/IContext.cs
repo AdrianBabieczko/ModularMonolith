91944170165cc63208c91dd443035c6c58572b3f
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-02-07 22:11:52 +0100

    Users requests

---
+++ src/Shared/Confab.Shared.Abstractions/Contexts/IContext.cs
@@ -0,0 +1,9 @@
+namespace Confab.Shared.Abstractions.Contexts
+{
+    public interface IContext
+    {
+        public string RequestId { get;}
+        public string TraceId { get; }
+        public IIdentityContext Identity { get;}
+    }
+}
