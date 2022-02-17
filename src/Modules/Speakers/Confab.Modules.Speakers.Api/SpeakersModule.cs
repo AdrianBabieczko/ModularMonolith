7ab69f7ed646db2b19cc869e9f221d6004944cd1
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-02-17 19:30:12 +0100

    Policies

--- src/Modules/Speakers/Confab.Modules.Speakers.Api/SpeakersModule.cs
+++ src/Modules/Speakers/Confab.Modules.Speakers.Api/SpeakersModule.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Confab.Modules.Speakers.Core;
 using Confab.Shared.Abstractions.Modules;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,8 @@ namespace Confab.Modules.Speakers.Api
         public const string BasePath = "speakers-module";
         public string Name { get; } = "Speakers";
         public string Path => BasePath;
+        
+        public IEnumerable<string> Policies { get; } = new[] {"speakers"};
 
         public void Register(IServiceCollection services)
         {
--- src/Modules/Users/Confab.Modules.Users.Api/UsersModule.cs
+++ src/Modules/Users/Confab.Modules.Users.Api/UsersModule.cs
@@ -1,15 +1,18 @@
+using System.Collections.Generic;
 using Confab.Modules.Users.Core;
 using Confab.Shared.Abstractions.Modules;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Confab.Modules.Users.Api.Controllers
+namespace Confab.Modules.Users.Api
 {
     public class UsersModule : IModule
     {
         public const string BasePath = "users-module";
         public string Name { get; } = "Users";
         public string Path => BasePath;
+
+        public IEnumerable<string> Policies { get; } = new[] {"users"};
 
         public void Register(IServiceCollection services)
         {
