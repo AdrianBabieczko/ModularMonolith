8fc44115b37beca8c54ee7d9d252b242bfdb3c46
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-03-02 22:21:39 +0100

    Update TicketSaleRepo

--- src/Modules/Tickets/Confab.Modules.Tickets.Core/DAL/Repositories/TicketSaleRepository.cs
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/DAL/Repositories/TicketSaleRepository.cs
@@ -1,41 +1,57 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Confab.Modules.Tickets.Core.Entities;
 using Confab.Modules.Tickets.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Confab.Modules.Tickets.Core.DAL.Repositories
 {
     internal class TicketSaleRepository : ITicketSaleRepository
     {
-        public Task<TicketSale> GetAsync(Guid id)
+        private readonly TicketsDbContext _context;
+        private readonly DbSet<TicketSale> _ticketSales;
+
+        public TicketSaleRepository(TicketsDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _ticketSales = _context.TicketSales;
         }
+
+        public Task<TicketSale> GetAsync(Guid id)
+            => _ticketSales.Include(x => x.Tickets).FirstOrDefaultAsync(x => x.Id == id);
 
         public Task<TicketSale> GetCurrencyForConferenceAsync(Guid conferenceId, DateTime now)
+            => _ticketSales
+                .Where(x => x.ConferenceId == conferenceId)
+                .OrderBy(x => x.From)
+                .Include(x => x.Tickets)
+                .LastOrDefaultAsync(x => x.From <= now && x.To >= now);
+
+        public async Task<IReadOnlyList<TicketSale>> BrowseForConferenceAsync(Guid conferenceId)
+            => await _ticketSales
+                .AsNoTracking()
+                .Where(x => x.ConferenceId == conferenceId)
+                .Include(x => x.Tickets)
+                .ToListAsync();
+
+        public async Task AddAsync(TicketSale ticketSale)
         {
-            throw new NotImplementedException();
+            await _ticketSales.AddAsync(ticketSale);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IReadOnlyList<TicketSale>> BrowseForConferenceAsync(Guid conferenceId)
+        public async Task UpdateAsync(TicketSale ticketSale)
         {
-            throw new NotImplementedException();
+            _ticketSales.Update(ticketSale);
+            await _context.SaveChangesAsync();
         }
 
-        public Task AddAsync(TicketSale ticketSale)
+        public async Task DeleteAsync(TicketSale ticketSale)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(TicketSale ticketSale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(TicketSale ticketSale)
-        {
-            throw new NotImplementedException();
+            _ticketSales.Remove(ticketSale);
+            await _context.SaveChangesAsync();
         }
     }
 }
