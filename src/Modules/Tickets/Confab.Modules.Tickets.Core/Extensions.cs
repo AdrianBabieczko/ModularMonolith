2d19125b0ccf7e0b8dfe161f320e998aa6bfc470
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-03-03 17:06:58 +0100

    Migrations

--- src/Modules/Tickets/Confab.Modules.Tickets.Core/Extensions.cs
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/Extensions.cs
@@ -1,5 +1,9 @@
 using System.Runtime.CompilerServices;
+using Confab.Modules.Tickets.Core.DAL;
+using Confab.Modules.Tickets.Core.DAL.Repositories;
+using Confab.Modules.Tickets.Core.Repositories;
 using Confab.Modules.Tickets.Core.Services;
+using Confab.Shared.Infrastructure.Postgres;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly:InternalsVisibleTo("Confab.Modules.Tickets.Api")]
@@ -9,6 +13,10 @@ namespace Confab.Modules.Tickets.Core
     {
         public static IServiceCollection AddCore(this IServiceCollection services)
             => services
-                .AddScoped<ITicketService, TicketsService>();
+                .AddScoped<ITicketService, TicketsService>()
+                .AddScoped<IConferenceRepository, ConferenceRepository>()
+                .AddScoped<ITicketRepository, TicketRepository>()
+                .AddScoped<ITicketSaleRepository, TicketSaleRepository>()
+                .AddPostgres<TicketsDbContext>();
     }
 }
