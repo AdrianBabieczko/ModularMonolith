d53b9e0faca937ee75331def20d1c1d2a9fad915
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-03-09 21:30:03 +0100

    Add exceptions

--- src/Modules/Tickets/Confab.Modules.Tickets.Core/Exceptions/TooManyTicketsException.cs
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/Exceptions/TooManyTicketsException.cs
@@ -4,7 +4,9 @@ using Confab.Shared.Abstractions.Exceptions;
 internal class TooManyTicketsException : ConfabException
 {
     public Guid ConferenceId { get; }
-    public TooManyTicketsException(Guid conferenceId) : base("Too many tickets would be generated fot the conference.")
+
+    public TooManyTicketsException(Guid conferenceId)
+        : base("Too many tickets would be generated fot the conference.")
     {
         ConferenceId = conferenceId;
     }
