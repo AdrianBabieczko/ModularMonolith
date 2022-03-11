caf94a8d5e175173a7dc8e32db252b08a48ac7a8
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-03-11 22:22:44 +0100

    Add handler

---
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/Events/External/Handlers/ConferenceCreatedHandler.cs
@@ -0,0 +1,7 @@
+namespace Confab.Modules.Tickets.Core.Events.External.Handlers
+{
+    internal sealed class ConferenceCreatedHandler // : IEventHandler<ConferenceCreated>
+    {
+        //TODO: implement this
+    }
+}
