2223fd7649cd978b5669a8f12adaaf2a4bb8b2d6
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-03-22 18:06:46 +0100

    Refactor conferences events

--- src/Modules/Conferences/Confab.Modules.Conferences.Core/Events/ConferenceCreated.cs
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Events/ConferenceCreated.cs
@@ -1,6 +1,7 @@
+using System;
 using Confab.Shared.Abstractions.Events;
 
-namespace Confab.Modules.Conferences.Messages.Events
+namespace Confab.Modules.Conferences.Core.Events
 {
     public record ConferenceCreated(Guid Id, string Name, int? ParticipantsLimit, DateTime From, DateTime To) : IEvent;
 }
